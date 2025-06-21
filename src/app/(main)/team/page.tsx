898207b7c9b737f2c27622e17bbcb2cf8bc10316
Mihir Barman <mihirbarman855@gmail.com>
2025-06-21 19:11:50 +0000

    use gradient background like this image.

--- src/app/(main)/team/page.tsx
+++ src/app/(main)/team/page.tsx
@@ -13,7 +13,7 @@ export default function TeamPage() {
       <div className="container py-16 md:py-24">
         <Tabs defaultValue="committee" className="w-full">
           <div className="w-full overflow-x-auto no-scrollbar pb-2">
-            <TabsList className="w-max sm:w-auto">
+            <TabsList className="w-max sm:w-auto sm:mx-auto">
               <TabsTrigger value="committee">Working Committee</TabsTrigger>
               <TabsTrigger value="core">Core Members</TabsTrigger>
               <TabsTrigger value="alumni">Alumni</TabsTrigger>
